3d2757f8c86b0f116fa7e2f8e6f885ea8ce4efc8
W E D Y <110571114+WedyDev@users.noreply.github.com>
2023-10-12 00:04:46 -0500

    4to Comit

--- dev_site/src/app/component/toolbar/toolbar-mostrar/toolbar-mostrar.component.ts
+++ dev_site/src/app/component/toolbar/toolbar-mostrar/toolbar-mostrar.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar-mostrar',
   templateUrl: './toolbar-mostrar.component.html',
   styleUrls: ['./toolbar-mostrar.component.css']
 })
-export class ToolbarMostrarComponent {
+export class ToolbarMostrarComponent implements OnInit  {
+  ngOnInit(): void {
+  }
 
+  downloadPdf() {
+    const pdfUrl = 'assets/pdf/CV_Delgado_Walter.pdf';
+
+    const link = document.createElement('a');
+    link.href = pdfUrl;
+    link.download = 'WalterDelgadoCV.pdf';
+
+    link.click();
+  }
 }
